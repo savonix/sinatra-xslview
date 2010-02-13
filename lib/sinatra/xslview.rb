ba95fe09ec0633661c04073bab5c5b8569b70ae8
Docunext <albert.lash@docunext.com>
2010-02-13 12:09:16 -0500

    added license and comment

--- lib/sinatra/xslview.rb
+++ lib/sinatra/xslview.rb
@@ -1,6 +1,17 @@
+###
+# Copyright: 2010 Savonix Corporation
+# Author:    Albert Lash
+# License:   MIT
+##
 require 'sinatra/base'
 
 module Sinatra
+  ###
+  # Renders output based upon XML and XSL
+  # In practice, I use Builder to create the XML to be transformed
+  # As such, I do not plan on adding support for parameters, like Rack-XSLView
+  # TODO: cache stylesheets
+  ##
   module XSLView
   def xslview(myxml,myxsl)
       xslt = ::XML::XSLT.new()
